86a14157feca6580b8dedada0784439903add6ea
lwalker <landen@example.com>
2023-08-24 06:36:14 -0500

    API request folder created

--- src/Components/Feed.jsx
+++ src/Components/Feed.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { Box, Stack, Typography } from '@mui/material';
-import Sidebar from './Sidebar';
+import { Sidebar, Videos } from './';
 
 const Feed = () => {
   return (
@@ -15,6 +15,18 @@ const Feed = () => {
       </Typography>
       </Box>
       
+      <Box p={2} sx={{ overflowY: 'auto',
+      height: '90vh', flex: 2}}>
+        <Typography variant="h4"
+        fontWeight="bold" mb={2} sx={{
+          color: 'white'
+        }}>
+          New <span style={{ color: '#F31503'}}
+          >videos</span>
+        </Typography>
+
+        <Videos videos={[]}/>
+      </Box>
     </Stack>
   )
 }
